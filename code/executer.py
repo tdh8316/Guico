fe82528b9be21475c811ff59664f86037318d65f
탁동혁 <tdh8316@naver.com>
2018-07-19 19:55:07 +0900

    bug fix: Sequence mismatch of script lexer, combiner

---
+++ code/executer.py
@@ -0,0 +1,54 @@
+from code.parser.script_parser import Parse
+from code.parser.script_lexer import Lexer
+from code.parser.combiner import Combiner
+
+# from core.actions import on_file_save as save
+from core.config import *
+
+
+class ConvertToC:
+
+    def __init__(self, code):
+        pass
+
+
+class PromptlyExecute:
+
+    def __init__(self, code):
+        print(f"\n{'=' * 50}\n{CONF['FILE_NAME']} Has started.\n{'=' * 50}")
+        self.code = code
+
+        for code in self.code:
+            if code[0] == "Print":
+                self. print(code[1]["str"])
+
+        print(f"\n{'=' * 50}\n{CONF['FILE_NAME']} Completed successfully.\n{'=' * 50}\n")
+
+    @staticmethod
+    def print(str_1: str, *args):
+        print(str_1.replace("\\n", "\n"), end=str())
+
+
+def interpreter(target, mode=None):
+    if mode is None:
+        try:
+            lexer = Lexer(target)
+        except IOError:
+            return
+        tokenize = lexer.lexer()
+
+        parser = Parse(target=tokenize, edges=lexer.edges())
+        if parser.leaves is not 1:
+            raw_scr = parser.get_token()[0]
+            # print(raw_scr)
+            connector = parser.get_token()[1]
+
+            array = Combiner(raw_scr, connector).combine()
+
+            # print(array)
+        else:
+            array = parser.get_token()[0]
+
+        # print(array)
+
+        PromptlyExecute(array)
---
+++ code/parser/combiner.py
@@ -0,0 +1,77 @@
+class _Combiner:
+
+    def __init__(self, code, conn):
+        self.code: dict = {"STARTS": [],
+                           "ENDS": []}
+        self.starts: list = []
+        self.ends: list = []
+        self.res: list = []
+        # print(conn)
+
+        for x in range(len(code)):
+            leaf_io_id = code[x][2]
+            leaf = [code[x][0], code[x][1]]
+            # print(leaf)
+            for y in range(len(conn)):
+                # 줄 시작점 == 잎 아웃풋 (시작점)
+                if conn[y][0] == leaf_io_id[1]:
+                    self.starts.append(leaf)
+                    self.code["STARTS"] = self.starts
+                # 줄 끝점 == 잎 인풋 (끝점)
+                elif conn[y][1] == leaf_io_id[0]:
+                    self.ends.append(leaf)
+                    self.code["ENDS"] = self.ends
+
+        del self.starts
+        del self.ends
+
+        self.code = self.code["STARTS"] + self.code["ENDS"]
+        [self.res.append(_) for _ in self.code if _ not in self.res]
+
+    def combine(self):
+        print(self.res)
+        return self.res
+
+
+# NOTE: UTC+9, 대한민국 표준시 기준, 2018년 7월 19일 오후 7시 01분 성공함!!!!!!
+
+class Combiner:
+
+    def __init__(self, src, connect):
+        self.src = src
+        self.connect = connect
+        self.replacing: list = []
+        self.result: list = []
+
+        # 엔트리 포인트 검색
+        for _ in self.src:
+            if _[0] == "Entry":
+                self.replacing.append(_)
+                break
+
+        # 엔트리 포인트에 연결된 노드 검색
+        for connect in self.connect:
+            if connect[0] == self.replacing[0][2][1]:
+                for code in self.src:
+                    if code[2][0] == connect[1]:
+                        self.replacing.append(code)
+
+        # 그 다음부터 끝까지..
+        for code in self.replacing:
+            if code[0] == "Entry":
+                continue
+            _socket_of_code = code[2]
+            for connect in self.connect:
+                # connect[0] : 인풋 잎 ID
+                if connect[0] == _socket_of_code[1]:
+                    for i in self.src:
+                        if connect[1] in i[2]:
+                            self.replacing.append(i)
+
+        # 결과 작성
+        for full_inf in self.replacing:
+            self.result.append([full_inf[0], full_inf[1]])
+
+    def combine(self):
+        # print(self.result)
+        return self.result
