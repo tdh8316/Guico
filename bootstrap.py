169769a10d4693dd73e0233010ba193e1592590f
탁동혁 <tdh8316@naver.com>
2018-08-04 14:42:40 +0900

    added splash screen

--- bootstrap.py
+++ bootstrap.py
@@ -1,9 +1,11 @@
 import os
 import sys
+import time
 
 import qdarkstyle
-from PyQt5.QtGui import QFontDatabase, QFont, QIcon
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFontDatabase, QFont, QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QSplashScreen, QProgressBar
 
 # from build_tools.compiler import interpreter
 from gui.window import MainForm
@@ -20,14 +22,36 @@ COMPILE_TEST = False
 def launch_window():
     app = QApplication([])
     QFontDatabase().addApplicationFont(r"NanumBarunpenR.ttf")
+    app.setFont(QFont("나눔바른펜", 11))
     # QFontDatabase().addApplicationFont(r"gui\resources\godoRounded L.ttf")
 
+    splash_pm = QPixmap("gui/resources/splash.png")
+    splash = QSplashScreen(splash_pm, Qt.WindowStaysOnTopHint)
+    splash.setWindowFlags(Qt.WindowStaysOnTopHint | Qt.FramelessWindowHint)
+    splash.setEnabled(False)
+    progressBar = QProgressBar(splash)
+    progressBar.setMaximum(100)
+    progressBar.setGeometry(0, splash_pm.height() - 25, splash_pm.width() + 50, 100)
+    splash.setFont(QFont("나눔바른펜", 17))
+    splash.setWindowTitle(" ".join([NAME, VERSION]))
+    splash.showMessage(f"{AUTHOR}'s {NAME} {VERSION}", Qt.AlignTop | Qt.AlignCenter, Qt.white)
+    splash.show()
+
+    for i in range(90):
+        progressBar.setValue(i)
+        t = time.time()
+        while time.time() < t + 0.025:
+            app.processEvents()
+
+        # Simulate something that takes time
+    time.sleep(1.25)
+
     app.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-    app.setFont(QFont("나눔바른펜", 11))
     app.setWindowIcon(QIcon(r"gui\resources\icon.ico"))
 
     root = MainForm()
     root.show()
+    splash.finish(root)
 
     if COMPILE_TEST:
         root.hide()
