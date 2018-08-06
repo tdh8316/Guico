25025d4176541495121dd187660a5b522f21731f
탁동혁 <tdh8316@naver.com>
2018-08-06 11:11:26 +0900

    remove obsolete types

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ from code_content.default import *
 NAME = "Guico"
 AUTHOR = "tdh8316@naver.com"
 TEAM = "TakturStudio"
-VERSION = "0.4fa31 빌드 128"
+VERSION = "0.4fa31 빌드 131"
 OPEN_SOURCE_LICENSE = open(r".\docs\LICENSE.txt", "r", encoding="utf-8").read()
 
 FILE_TYPES = f"{NAME} script files (*.gvs);;" \
