8153482f78348ea0236efb2cb0dc47f4ed0ed264
탁동혁 <tdh8316@naver.com>
2018-08-16 13:23:37 +0900

    bug fix : is_key_pressed 함수가 MOUSEMOTION 이벤트를 처리하던 문제를 해결했습니다.

--- Engine/event_manager.py
+++ Engine/event_manager.py
@@ -1,5 +1,7 @@
 import Engine
 
+latest_key = None
+
 
 def Event(event):
     if event.type == (Engine.KEYDOWN or Engine.KEYUP):
@@ -12,8 +14,13 @@ def Event(event):
 
 
 def is_key_pressed(key: str):
+    global latest_key
+    if Engine.event_type == Engine.MOUSEMOTION or Engine.event_type == Engine.ACTIVEEVENT:
+        if ord(key) == latest_key:
+            return True
+    elif Engine.event_type == Engine.KEYUP:
+        latest_key = None
     if ord(key) == Engine.pressed_key:
+        latest_key = ord(key)
         return True
     return False
-
-
