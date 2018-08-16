9701fc820afacf6c34ec60cff1b9fbd73d96d858
탁동혁 <tdh8316@naver.com>
2018-08-16 19:34:43 +0900

    bug fix : 키를 입력 받을 때 다른 이벤트에 의해 방해받던 문제가 향상된 입력 유효성 검사를 통해 해결되었습니다.

--- Engine/event_manager.py
+++ Engine/event_manager.py
@@ -1,26 +1,16 @@
 import Engine
 
-latest_key = None
-
 
 def Event(event):
-    if event.type == (Engine.KEYDOWN or Engine.KEYUP):
-        Engine.pressed_key = event.key
-    elif event.type == Engine.QUIT:
+    if event.type == Engine.QUIT:
         Engine.loop = False
-    else:
+    elif event.type == Engine.KEYUP:
         Engine.pressed_key = None
     Engine.event_type = event.type
 
 
 def is_key_pressed(key: str):
-    global latest_key
-    if Engine.event_type == Engine.MOUSEMOTION or Engine.event_type == Engine.ACTIVEEVENT:
-        if ord(key) == latest_key:
-            return True
-    elif Engine.event_type == Engine.KEYUP:
-        latest_key = None
-    if ord(key) == Engine.pressed_key:
-        latest_key = ord(key)
+    if Engine.key.get_pressed()[ord(key)]:
+        Engine.pressed_key = key
         return True
     return False
