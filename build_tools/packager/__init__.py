6e19dc1da2f9fcc7fc068e02e390825a9cd4f562
탁동혁 <tdh8316@naver.com>
2018-08-18 22:10:48 +0900

    변경된 사항 적용

--- build_tools/packager/__init__.py
+++ build_tools/packager/__init__.py
@@ -5,7 +5,6 @@ import shutil
 from PyQt5.QtWidgets import QMessageBox
 
 from core.config import *
-from build_tools.packager.python_environment import MakePyEnv
 
 
 parent = None
@@ -17,24 +16,5 @@ def initialize(_parent):
 
 
 def packaging_windows(bsd):
-    if (bsd or CONF['SOURCE_PATH'] or CONF['FILE_PATH']) is None:
-        QMessageBox.critical(None, "패키징 실패", "패키징 실패!")
-        return False
-    try:
-        parent.log.appendPlainText(f"{CONF['FILE_PATH']} 에 대한 패키징 시작...")
-        MakePyEnv(basedir=bsd)
-        shutil.copy("NanumBarunpenR.ttf", os.path.join(bsd, "NanumBarunpenR.ttf"))
-        shutil.copytree("./Engine/", bsd + "/Engine/")
-        with open(os.path.join(bsd, "bootstrapper.cmd"), "w") as script:
-            script.write("@ECHO OFF\n")
-            script.write(f".\Binaries\python.exe "
-                         r".\Binaries\\" +
-                         os.path.join(CONF['SOURCE_PATH'].replace("/", "\\").split('\\')[-1].split('.')[0]))
-        QMessageBox.information(None, "패키징 완료",
-                                f"{CONF['FILE_PATH']} 의 Windows 에 대한 패키징을 완료했습니다!")
-        os.system(f"explorer {bsd}")
-
-    except Exception as e:
-        QMessageBox.critical(
-            None, "패키징 실패", f"패키징 실패!\n{e}\n{sys.exc_info()}\n{sys.exc_info()[2].tb_lineno}")
+    parent.log.appendPlainText(f"{CONF['FILE_PATH']} 에 대한 패키징 시작...")
 
