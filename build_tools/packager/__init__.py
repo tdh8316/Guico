a0ca6e487937431a45a99b8f3fddfe6e699015c0
탁동혁 <tdh8316@naver.com>
2018-08-19 10:16:20 +0900

    자동화된 pyinstalelr 설치가 추가되었습니다.

--- build_tools/packager/__init__.py
+++ build_tools/packager/__init__.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import subprocess
 
 from PyQt5.QtWidgets import QMessageBox
 
@@ -16,5 +17,13 @@ def initialize(_parent):
 
 
 def packaging_windows(bsd):
+    parent.dock_log.show()
+    if not os.path.isfile(os.path.join(os.path.dirname(os.environ["PYTHON"]), "Scripts", "pyinstaller.exe")):
+        parent.log.appendPlainText("installing pyinstaller...")
+        p = subprocess.Popen([os.environ["PYTHON"], "-m", "pip", "install", "pyinstaller"],
+                             stdout=subprocess.PIPE)
+        (output, err) = p.communicate()
+        p.wait()
+        parent.log.appendPlainText(f"installed pyinstaller successfully {output}")
     parent.log.appendPlainText(f"{CONF['FILE_PATH']} 에 대한 패키징 시작...")
 
