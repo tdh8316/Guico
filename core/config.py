08dec5514261fd0f245d5561501e478059de2554
탁동혁 <tdh8316@naver.com>
2018-08-24 17:24:24 +0900

    updated version

--- core/config.py
+++ core/config.py
@@ -3,7 +3,7 @@ import os
 NAME = "Guico"
 AUTHOR = "tdh8316@naver.com"
 TEAM = "TakturStudio"
-VERSION = "3.04 Saucy Stone"
+VERSION = "3.221 Saucy Stone"
 TMP_PATH = os.path.join(os.path.expanduser("~"), f".{NAME}")
 PREF_FILE = os.path.join(TMP_PATH, ".pref")
 PERSON_IMAGE = (os.path.join(os.getcwd(), "gui", "resources", "person.png")
